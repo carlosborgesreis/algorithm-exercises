234adc3e2476aecdbcc75ef8b661d157441c78b7
carlosborgesreis <carlosborges161@gmail.com>
2024-03-20 14:41:23 -0300

    changing sum logic

--- python/least_squares_fitting.py
+++ python/least_squares_fitting.py
@@ -7,22 +7,17 @@ class LeastSquaresFitting:
         Least Squares Fitting calculation: 
         A line equation y = mx + b
         m is the slope of the function, given by 
-            m = (N * sum(xy) - sum(x)sum(y))/(N * sum(x^2))
+            m = (N * sum(xy) - sum(x)sum(y))/(N * sum(x^2) - sum(x)^2)
         b is the intercept, given by
             b = (sum(y) - m * sum(x))/N
         N is the number of data points
         '''
         if not series: return []
 
-        x_axis, y_axis = zip(*series)
-
-        sum_x  = sum(x_axis)
-        sum_x2 = sum([x**2 for x in x_axis])
-        sum_y  = sum(y_axis)
-        sum_xy = sum([x*y for x, y in series])
+        sum_x, sum_y, sum_x_sq, sum_xy = map(sum, zip(*[(x, y, x*x, x*y) for x, y in series]))
 
         N = len(series)
-        m = (N * sum_xy - sum_x * sum_y) / (N * sum_x2)
+        m = (N * sum_xy - sum_x * sum_y) / ((N * sum_x_sq) - sum_x ** 2)
         b = (sum_y - m * sum_x) / N
 
         return [(x, round(m*x + b, round_by)) for x, _ in series]
